ad8bd53445178cb056d4758830c9935b4de91c13
slyasminn <y.leandro@escola.pr.gov.br>
2026-06-02 12:16:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
+body {
+    font-family: Arial, sans-serif;
+    font-size: 18px;
+    line-height: 1.8;
+    margin: 20px;
+    background-color: #ffffff;
+    color: #000000;
+}
 
+header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex-wrap: wrap;
+}
+
+.acessibilidade button {
+    padding: 10px;
+    margin: 5px;
+    font-size: 18px;
+    cursor: pointer;
+}
+
+button:focus {
+    outline: 3px solid #005fcc;
+}
+
+h1, h2 {
+    color: #003366;
+}
